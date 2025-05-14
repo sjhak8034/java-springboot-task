a1c641f626ce16ab7822121003e5e27f6e575615
thdwj <thdwjdgkr123@naver.com>
2025-05-14 22:28:10 +0900

    git message

--- src/main/java/com/example/javaspringboottask/JavaSpringbootTaskApplication.java
+++ src/main/java/com/example/javaspringboottask/JavaSpringbootTaskApplication.java
@@ -2,8 +2,10 @@ package com.example.javaspringboottask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JavaSpringbootTaskApplication {
 
     public static void main(String[] args) {
