cfe0d1daf2cb089f85b9f219483297e1bf6011a8
thdwj <thdwjdgkr123@naver.com>
2025-05-14 15:55:13 +0900

    first commit

---
+++ src/main/java/com/example/javaspringboottask/JavaSpringbootTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringboottask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaspringboottask/JavaSpringbootTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringboottask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
